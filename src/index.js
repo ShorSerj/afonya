a07e6fa96a70e2fcd3757f582069cd89cd99db7c
ShorSerj <shorser8@gmail.com>
2022-02-23 11:52:21 +0300

    complete visible params options

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const params = document.querySelectorAll('.param__title ')
 document.addEventListener('click', function(event){
     let activeElement = document.querySelector('.param_active') 
     let paramOptions = document.querySelector('.param__options-wrapper_visible')
-    console.log('asdsad',activeElement)
+
     if(activeElement && !activeElement?.contains(event.target)){
         activeElement?.classList.remove('param_active')
         paramOptions?.classList.remove('param__options-wrapper_visible')
@@ -15,14 +15,18 @@ document.addEventListener('click', function(event){
 })
 
 params.forEach((item) => {
-        item.addEventListener('click', function(){
+    item.addEventListener('click', function(e){
         let activeElement = document.querySelector('.param_active') 
         activeElement?.classList.remove('param_active')
         let paramOptions = document.querySelector('.param__options-wrapper_visible')
+
         paramOptions?.classList.remove('param__options-wrapper_visible')
-        
-        item.parentElement.querySelector('.param__options-wrapper').classList.toggle('param__options-wrapper_visible')
-        item.parentElement.classList.toggle('param_active')
+        activeElement?.classList.remove('param_active')
+
+        if(!activeElement?.contains(e.target)){ 
+            item.parentElement.querySelector('.param__options-wrapper').classList.add('param__options-wrapper_visible')
+            item.parentElement.classList.add('param_active')
+        }     
     })
 })
 
